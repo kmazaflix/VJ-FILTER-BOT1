7f1cfffada0d93e752e6b9e27ae8b793ee4e5ab0
kmazaflix <110471285+kmazaflix@users.noreply.github.com>
2024-06-23 22:55:25 +0530

    Update share_text.py

--- plugins/Extra/share_text.py
+++ plugins/Extra/share_text.py
@@ -16,7 +16,7 @@ async def share_text(client, message):
     else:
         await vj.reply_text(
             text=f"**Notice:**\n\n1. Send Any Text Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/StreamfFlix_Support")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/StreamFlix_Support")]])
             )                                                   
         return
     await vj.reply_text(
